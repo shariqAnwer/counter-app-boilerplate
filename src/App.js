4b8236545369c8e475c76b2d1a10a2340a8f7084
Md Sharique Anwer <shariqueanwer002@gmail.com>
2022-06-17 02:02:07 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState(0);
+
+  const handleInc = (value) => {
+    setData(data + value);
+  };
+
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h1>Counter App</h1>
+      <h2 data-testid="counter-value">{data}</h2>
+      <button
+        data-testid="counter-decrement-button"
+        onClick={() => handleInc(-1)}
+      >
+        -
+      </button>
+      <button
+        data-testid="counter-increment-button"
+        onClick={() => handleInc(1)}
+      >
+        +
+      </button>
     </div>
   );
 }
